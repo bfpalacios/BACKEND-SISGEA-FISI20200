95243aa95d3c0b329427aab410298c3c91959cef
Mario <mario.cortez1@unmsm.edu.pe>
2020-03-01 14:43:44 -0500

    fix: mapper auth

--- sisgea-seguridad/src/main/java/edu/taller/sisgea/seguridad/mapper/IAuthMapper.java
+++ sisgea-seguridad/src/main/java/edu/taller/sisgea/seguridad/mapper/IAuthMapper.java
@@ -2,9 +2,12 @@ package edu.taller.sisgea.seguridad.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import edu.taller.sisgea.seguridad.model.CriterioBusquedaAutorizacion;
 import edu.taller.sisgea.seguridad.model.Recurso;
 
+@Mapper
 public interface IAuthMapper {
 	
 	Boolean autenticarUsuario(CriterioBusquedaAutorizacion criterio);
