ead6f3b9c0aad6e84339b02fc89f6dc3507eae2e
CristhianAlvarez1 <47466640+CristhianAlvarez1@users.noreply.github.com>
2020-02-29 19:24:03 -0500

    fix: más detalles

--- sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/model/Solicitante.java
+++ sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/model/Solicitante.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ob.commons.spring.validation.validation.CaracterRequerido;
+import ob.commons.spring.validation.validation.IdNumerico;
 
 @Data
 @Builder
@@ -13,18 +14,21 @@ import ob.commons.spring.validation.validation.CaracterRequerido;
 public class Solicitante {
 	
 	private Integer idSolicitante;
-	private Integer dni;
+	@CaracterRequerido(minLength = 8, maxLength = 8, regexpPattern = "[0-9]+")
+	private String dni;
 	@CaracterRequerido(maxLength = 40)
 	private String nombres;
 	@CaracterRequerido(maxLength = 40)
 	private String apellidoPaterno;
 	@CaracterRequerido(maxLength = 40)
 	private String apellidoMaterno;
+	@IdNumerico(maxRange = 999999999, minRange = 99999999)
 	private Integer celular;
 	private String email;
 	private String tipoSolicitante;
 	private String descripcionTipoSolicitante;
-	private Integer base;
+	private String base;
+	private String descripcionBase;
 	private String escuela;
 	private String descripcionEscuela;
 
